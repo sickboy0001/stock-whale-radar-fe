f723d8b773ffcdcf5aaeef104d0c551112875c62
sickboy0001 <syunjyu0001@gmail.com>
2026-05-02 05:38:40 +0900

    fix /web etc

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,4 +1,4 @@
-import { createClient } from "@libsql/client/web";
+import { createClient } from "@libsql/client";
 import { drizzle } from "drizzle-orm/libsql";
 import * as schema from "./schema";
 
